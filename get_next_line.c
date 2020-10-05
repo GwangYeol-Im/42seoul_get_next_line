ae32742d45319410ee4affc73f686f8e93c94fe8
Kyle <mkitigy@gmail.com>
2020-10-05 22:29:15 +0900

    modify .c files

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,73 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 22:03:58 by gim               #+#    #+#             */
-/*   Updated: 2020/10/04 22:24:43 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/05 22:22:43 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(int fd, char **line)
+#include "get_next_line.h"
+
+int					ft_get_line(char **backup, char **line)
 {
+	int				idx;
+	char			*temp;
+	char			*split;
+	
+	idx = 0;
+	temp = *backup;
+	while (temp[idx] != '\n')
+	{
+		if (!temp[idx++])
+			return (0);
+	}
+	split = &temp[idx];
+	*split = '\0';
+	*line = ft_strdup(*backup);
+	*backup = ft_strdup(split + 1);
+	return (1);
+}
+
+int					ft_read_file(int fd, char **line, char *buffer, char **backup)
+{
+	int				re;
+
+	while ((re = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (!*backup)
+			*backup = ft_strdup(buffer);
+		else
+			*backup = ft_strjoin(*backup, buffer);
+		if (ft_get_line(backup, line))
+			break ;
+	}
+	return (re > 0 ? 1 : 0);
+}
+
+int					get_next_line(int fd, char **line)
+{
+	static char		*backups[FD_MAX];
+	char			*buffer;
+	int				idx;
+	int				re;
+
+	if (!line || (fd < 0 || fd > FD_MAX) || (read(fd, backups[fd], 0) < 0) \
+		|| !(buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
+		return (-1);
+	if (backups[fd])
+		if (ft_get_line(&backups[fd], line))
+			return (1);
+	idx = 0;
+	while (idx <= BUFFER_SIZE)
+		buffer[idx++] = '\0';
+	re = ft_read_file(fd, line, buffer, &backups[fd]);
+	free(buffer);
+	if (re != 0 || backups[fd] == NULL || backups[fd][0] == '\0')
+	{
+		if (!re && *line)
+			*line = NULL;
+		return (re);
+	}
+	*line = backups[fd];
+	backups[fd] = NULL;
+	return (1);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,60 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 22:04:02 by gim               #+#    #+#             */
-/*   Updated: 2020/10/04 22:05:32 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/05 22:27:56 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	int		len;
+	int		idx;
+	char	*dest;
+
+	len = ft_strlen(s1);
+	dest = malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (0);
+	idx = 0;
+	while (idx <= len)
+	{
+		dest[idx] = s1[idx];
+		idx++;
+	}
+	return (dest);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len;
+	int		idx;
+	char	*new_s1;
+	char	*new_s2;
+	char	*join;
+
+	new_s1 = (char *)s1;
+	new_s2 = (char *)s2;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	join = malloc(sizeof(char) * (len + 1));
+	if (!join)
+		return (0);
+	idx = 0;
+	while (*s1)
+		join[idx++] = *s1++;
+	while (*s2)
+		join[idx++] = *s2++;
+	join[idx] = '\0';
+	return (join);
+}
