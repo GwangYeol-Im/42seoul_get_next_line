4209b69aef7770970792d9925940357a659a3fe9
Kyle <mkitigy@gmail.com>
2020-10-05 22:29:01 +0900

    modify header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 22:03:54 by gim               #+#    #+#             */
-/*   Updated: 2020/10/04 22:05:36 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/05 22:28:07 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+# define FD_MAX 1024
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+
+#endif
