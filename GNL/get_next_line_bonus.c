d1e33f73e950dad9555161f27418d1f439f61914
Kyle <mkitigy@gmail.com>
2020-10-11 16:24:57 +0900

    modify functions

--- GNL/get_next_line_bonus.c
+++ GNL/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 23:29:47 by gim               #+#    #+#             */
-/*   Updated: 2020/10/10 01:06:09 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/11 16:24:33 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ int				ft_read_file(int fd, char *buffer, char **line, char **backup)
 	int			re;
 	char		*temp;
 
-	ft_memset(buffer, 0, BUFFER_SIZE + 1);
-	while ((re = read(fd, buffer, BUFFER_SIZE)) > 0)
+	ft_memset(buffer, 0, BS + 1);
+	while ((re = read(fd, buffer, BS)) > 0)
 	{
 		if (!*backup)
 			*backup = ft_strdup(buffer);
@@ -51,7 +51,7 @@ int				ft_read_file(int fd, char *buffer, char **line, char **backup)
 		}
 		if (ft_get_line(backup, line))
 			return (1);
-		ft_memset(buffer, 0, BUFFER_SIZE + 1);
+		ft_memset(buffer, 0, BS + 1);
 	}
 	return (0);
 }
@@ -63,14 +63,13 @@ int				get_next_line(int fd, char **line)
 	int			re;
 
 	if (!line || (fd < 0 || fd > FD_MAX) || (read(fd, backups[fd], 0) < 0) \
-	|| (BUFFER_SIZE <= 0))
-		return (-1);
-	if (!(buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	|| (BS <= 0) || !(buffer = malloc(sizeof(char) * (BS + 1))))
 		return (-1);
 	if (backups[fd])
 		if (ft_get_line(&backups[fd], line))
 			return (1);
 	re = ft_read_file(fd, buffer, line, &backups[fd]);
+	free(buffer);
 	if (!re)
 	{
 		if (backups[fd])
